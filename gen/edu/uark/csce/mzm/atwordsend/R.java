ba0efb4beb5b5a95d748bc725fd7c48b6bf1705f
DowncastHarp <msnlietzke@yahoo.com>
2014-10-16 17:09:52 -0500

    Added Activities, Made Some Menus
    Added activities, made layouts for main menu and friends list.  Need to
    look into using LoginActivity provided by eclipse

--- gen/edu/uark/csce/mzm/atwordsend/R.java
+++ gen/edu/uark/csce/mzm/atwordsend/R.java
@@ -1080,6 +1080,14 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
+        public static final int AddFriendButton=0x7f05003d;
+        public static final int BackButton=0x7f05003e;
+        public static final int FriendListView=0x7f05003c;
+        public static final int FriendsListButton=0x7f050042;
+        public static final int GameTitle=0x7f05003f;
+        public static final int MatchFindButton=0x7f050041;
+        public static final int SettingsButton=0x7f050043;
+        public static final int ViewGamesListButton=0x7f050040;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1091,7 +1099,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f050044;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1192,13 +1200,27 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_login=0x7f030018;
-        public static final int activity_main_menu=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int activity_add_friend=0x7f030018;
+        public static final int activity_friends_list=0x7f030019;
+        public static final int activity_games_list=0x7f03001a;
+        public static final int activity_login=0x7f03001b;
+        public static final int activity_main_menu=0x7f03001c;
+        public static final int activity_match_find=0x7f03001d;
+        public static final int activity_settings=0x7f03001e;
+        public static final int activity_view_friend=0x7f03001f;
+        public static final int activity_view_game=0x7f030020;
+        public static final int support_simple_spinner_dropdown_item=0x7f030021;
     }
     public static final class menu {
-        public static final int login=0x7f0c0000;
-        public static final int main_menu=0x7f0c0001;
+        public static final int add_friend=0x7f0c0000;
+        public static final int friends_list=0x7f0c0001;
+        public static final int game=0x7f0c0002;
+        public static final int games_list=0x7f0c0003;
+        public static final int login=0x7f0c0004;
+        public static final int main_menu=0x7f0c0005;
+        public static final int match_find=0x7f0c0006;
+        public static final int settings=0x7f0c0007;
+        public static final int view_friend=0x7f0c0008;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1242,9 +1264,23 @@ containing a value of this type.
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
+        public static final int add_friend=0x7f0a0016;
         public static final int app_name=0x7f0a000d;
+        public static final int back=0x7f0a0017;
+        public static final int friends_list=0x7f0a001d;
         public static final int hello_world=0x7f0a000e;
+        public static final int match_find=0x7f0a001c;
+        public static final int settings=0x7f0a001e;
+        public static final int title_activity_add_friend=0x7f0a0019;
+        public static final int title_activity_friends_list=0x7f0a0011;
+        public static final int title_activity_game=0x7f0a0014;
+        public static final int title_activity_games_list=0x7f0a0013;
         public static final int title_activity_login=0x7f0a0010;
+        public static final int title_activity_match_find=0x7f0a0015;
+        public static final int title_activity_settings=0x7f0a0012;
+        public static final int title_activity_view_friend=0x7f0a0018;
+        public static final int title_game=0x7f0a001a;
+        public static final int view_games_list=0x7f0a001b;
     }
     public static final class style {
         /** 
