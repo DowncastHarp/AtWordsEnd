ed9edffc6abe8aeac965476796f45927e86daf5e
DowncastHarp <msnlietzke@yahoo.com>
2014-11-07 16:42:47 -0600

    Added Connectivity Between UIs
    xml only, Matt's has the java

--- gen/edu/uark/csce/mzm/atwordsend/R.java
+++ gen/edu/uark/csce/mzm/atwordsend/R.java
@@ -1091,7 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05005c;
+        public static final int action_settings=0x7f05005d;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int addFriendButton=0x7f050040;
         public static final int addFriendEditText=0x7f05003f;
@@ -1101,7 +1101,7 @@ containing a value of this type.
         public static final int button1=0x7f050053;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int countdownTimer=0x7f050058;
+        public static final int countdownTimer=0x7f050059;
         public static final int default_activity_button=0x7f050028;
         public static final int deleteFriendButton=0x7f050057;
         public static final int dialog=0x7f05000e;
@@ -1133,9 +1133,10 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int opponentName=0x7f050058;
         public static final int passwordEditText=0x7f05004a;
         public static final int passwordTitle=0x7f050049;
-        public static final int playerWordEditText=0x7f050059;
+        public static final int playerWordEditText=0x7f05005a;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1161,14 +1162,14 @@ containing a value of this type.
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
-        public static final int submitButton=0x7f05005a;
+        public static final int submitButton=0x7f05005b;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
-        public static final int usedWords=0x7f05005b;
+        public static final int usedWords=0x7f05005c;
         public static final int usernameEditText=0x7f050048;
         public static final int usernameTitle=0x7f050047;
         public static final int viewGamesListButton=0x7f05004d;
