b2a3e5d26733c3b10e8b8aa4867a47d646927383
megoff <megoff@uark.edu>
2014-12-06 14:32:51 -0600

    Added dict
    It is here.

--- src/edu/uark/csce/mzm/atwordsend/AddFriendActivity.java
+++ src/edu/uark/csce/mzm/atwordsend/AddFriendActivity.java
@@ -1,12 +1,16 @@
 package edu.uark.csce.mzm.atwordsend;
 
 import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
-public class AddFriendActivity extends Activity {
+public class AddFriendActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +18,37 @@ public class AddFriendActivity extends Activity {
 		setContentView(R.layout.activity_add_friend);
 	}
 	
+	public void sendRequest(View v){
+
+		ContentResolver cr = getContentResolver();
+		ContentValues values = new ContentValues();
+		values.put(AddRequestContentProvider.KEY_NAME, ((EditText) findViewById(R.id.addFriendEditText)).getText().toString());
+		cr.insert(AddRequestContentProvider.CONTENT_URI, values);
+
+		getLoaderManager().restartLoader(0, null, this);
+	}
+	
 	public void close(View v){
 		this.finish();
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		// Recieved requests stuff
+		
+		//Sent requests stuff
+		
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		// TODO Auto-generated method stub
+		
 	}
 }
