eb8a5741d20ffa295b24321245a1cbfe6ad72972
megoff <megoff@uark.edu>
2014-12-10 19:53:10 -0600

    Finished GamesListActivity

--- src/edu/uark/csce/mzm/atwordsend/GamesListActivity.java
+++ src/edu/uark/csce/mzm/atwordsend/GamesListActivity.java
@@ -1,21 +1,25 @@
 package edu.uark.csce.mzm.atwordsend;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import android.app.Activity;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class GamesListActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
 
+	private static final int GAME_ACTIVITY = 1;
 	private ArrayList<Game> games;
 	private GamesAdapter gamesAdapter;
 	
@@ -29,8 +33,34 @@ public class GamesListActivity extends Activity implements LoaderManager.LoaderC
 		
 	    ListView listView = (ListView) findViewById(R.id.gamesListView);
 	    listView.setAdapter(gamesAdapter);
+
+	    listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+	    	@Override
+	    	public void onItemClick(AdapterView<?> parent, final View view, int pos, long id){
+	    		final Game item = (Game) parent.getItemAtPosition(pos);
+
+	    		Intent intent = new Intent(GamesListActivity.this, ViewGameActivity.class);
+	    		intent.putExtra("Opponent", item.getOpponent());
+	    		intent.putExtra("myTurn", item.getMyTurn());
+	    		intent.putExtra("Words", item.getUsedWords());
+	    		startActivityForResult(intent, GAME_ACTIVITY);
+	    	}
+	    });
+	    
+		getLoaderManager().restartLoader(0, null, this);
 	}
 	
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		getLoaderManager().restartLoader(0, null, this);
+		switch(resultCode){
+			case GAME_ACTIVITY:
+				getLoaderManager().restartLoader(0, null, this);
+				break;
+			
+			default:
+				break;
+		}
+	}
 	public void close(View v){
 		GamesListActivity.this.finish();
 	}
@@ -44,18 +74,19 @@ public class GamesListActivity extends Activity implements LoaderManager.LoaderC
 	@Override
 	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
 		
-		int Name_Index = data.getColumnIndexOrThrow(GameContentProvider.KEY_OPPONENT);
-		int Wins_Index = data.getColumnIndexOrThrow(GameContentProvider.KEY_TURN);
-		int Losses_Index = data.getColumnIndexOrThrow(GameContentProvider.KEY_WORDS);
+		int Opponent_Index = data.getColumnIndexOrThrow(GameContentProvider.KEY_OPPONENT);
+		int Turn_Index = data.getColumnIndexOrThrow(GameContentProvider.KEY_TURN);
+		int Words_Index = data.getColumnIndexOrThrow(GameContentProvider.KEY_WORDS);
 		
 		games.clear();
 		
 		while (data.moveToNext()) {
 			
+			ArrayList<String> list = new ArrayList<String>(Arrays.asList(data.getString(Words_Index).split(";")));
 			
-			Game newitem = new Game(data.getString(Name_Index),
-									data.getInt(Wins_Index),
-									data.getInt(Losses_Index));
+			Game newitem = new Game(data.getString(Opponent_Index),
+								   (data.getInt(Turn_Index) == 1 ? true : false),
+									list);
 			games.add(newitem);
 		}
 
